07aeadd99f9165878ecd3bf039337b1c9d6ca647
hansveb <hans.vandenbogaerde@gmail.com>
2021-02-28 20:00:02 +0100

    for demonstration in Spring 2021

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Python Advanced after update!'
+    return 'Hello Python Advanced after update in February 2021!'
 
 
 if __name__ == '__main__':
