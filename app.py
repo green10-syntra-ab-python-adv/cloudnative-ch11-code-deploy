52750a093e11cb8f437e54e899f87620acbb800c
HansVdb <hans.vandenbogaerde@gmail.com>
2022-03-27 15:50:24 +0200

    March 2022!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Python Advanced after update in February 2021!'
+    return 'Hello Python Advanced after update in March 2022!'
 
 
 if __name__ == '__main__':
